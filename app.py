db90e42737ff16fbca84690fcc7a1d8b03a31a96
황단우 <puppy@hwangdan-uui-MacBookPro.local>
2025-07-08 15:40:59 +0900

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
 from uuid import uuid4
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -37,4 +38,5 @@ def disconnect():
         del users[request.sid]
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
