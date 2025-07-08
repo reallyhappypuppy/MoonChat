7aeb05d00594c526d4a3c79aee6b9c793852bb1a
황단우 <puppy@hwangdan-uui-MacBookPro.local>
2025-07-09 07:57:10 +0900

    bug

--- app.py
+++ app.py
@@ -1,42 +1,49 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 from uuid import uuid4
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
-
-# 닉네임 저장: 세션별 고유 ID
-users = {}
+online_users = set()
+users = {}  # Maps session ID to user info
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @socketio.on('connect')
-def connect():
+def handle_connect():
     user_id = str(uuid4())
     users[request.sid] = {'id': user_id, 'nickname': '익명'}
+    online_users.add(request.sid)
     print(f'User connected: {user_id}')
+    emit('user_count', len(online_users), broadcast=True)
 
 @socketio.on('set_nickname')
 def set_nickname(nick):
-    users[request.sid]['nickname'] = nick
+    if request.sid in users:
+        users[request.sid]['nickname'] = nick
 
 @socketio.on('message')
 def handle_message(msg):
-    nickname = users[request.sid]['nickname']
-    full_msg = f"{nickname}: {msg}"
-    print(full_msg)
-    send(full_msg, broadcast=True)
+    if request.sid in users:
+        nickname = users[request.sid]['nickname']
+        full_msg = f"{nickname}: {msg}"
+        print(full_msg)
+        send(full_msg, broadcast=True)
 
 @socketio.on('disconnect')
-def disconnect():
+def handle_disconnect():
     if request.sid in users:
         print(f"User disconnected: {users[request.sid]['nickname']}")
         del users[request.sid]
+    online_users.discard(request.sid)
+    emit('user_count', len(online_users), broadcast=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    import eventlet
+    import eventlet.wsgi
+    port = int(os.environ.get("PORT", 5001))
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', port)), app)
