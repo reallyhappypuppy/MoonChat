a6a0bbb14e6f3fc7be595dad8c76610e10d3ce4b
황단우 <puppy@hwangdan-uui-MacBookPro.local>
2025-07-08 15:21:32 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send
+from uuid import uuid4
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+# 닉네임 저장: 세션별 고유 ID
+users = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def connect():
+    user_id = str(uuid4())
+    users[request.sid] = {'id': user_id, 'nickname': '익명'}
+    print(f'User connected: {user_id}')
+
+@socketio.on('set_nickname')
+def set_nickname(nick):
+    users[request.sid]['nickname'] = nick
+
+@socketio.on('message')
+def handle_message(msg):
+    nickname = users[request.sid]['nickname']
+    full_msg = f"{nickname}: {msg}"
+    print(full_msg)
+    send(full_msg, broadcast=True)
+
+@socketio.on('disconnect')
+def disconnect():
+    if request.sid in users:
+        print(f"User disconnected: {users[request.sid]['nickname']}")
+        del users[request.sid]
+
+if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0", port=5001)
